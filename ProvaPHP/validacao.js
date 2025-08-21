5d11bf541f5d093490d8bd3d19747a8a9f129460
Kelzita <raquelfernandesbriyoidjdj@gmail.com>
2025-08-21 17:03:44 -0300

    Validações
    Validação para Usuário e Fornecedor

--- ProvaPHP/validacao.js
+++ ProvaPHP/validacao.js
@@ -1,11 +1,10 @@
-let nome = document.getElementById("nome");
-let email = document.getElementById("email");
-let senha = document.getElementById("senha");
-let id_perfil = document.getElementById("id_perfil");
-
 function ValidacaodoFormulario(event) {
     event.preventDefault(); // Impede envio até a validação passar
 
+    let nome = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let senha = document.getElementById("senha");
+    let id_perfil = document.getElementById("id_perfil");
 
     // ======= Campos não podem estar vazios =======
     if (nome.value.trim() === "") {
@@ -67,3 +66,72 @@ function ValidacaodoFormulario(event) {
 }
 
 document.querySelector("form").addEventListener("submit", ValidacaodoFormulario);
+
+
+
+
+
+//========= VALIDAÇÃO DO FORNECEDOR ==========
+
+function ValidacaodoFormularioFornecedor(event) {
+    event.preventDefault(); // Impede envio até a validação passar
+
+    // ======= Campos não podem estar vazios =======
+    if (nome_fornecedor.value.trim() === "") {
+        alert("O campo Nome deve ser preenchido!");
+        nome_fornecedor.focus();
+        return false;
+    }
+
+    if (endereco.value.trim() === "") {
+        alert("O campo Endereço deve ser preenchido!");
+        endereco.focus();
+        return false;
+    }
+
+    if (telefone.value.trim() === "") {
+        alert("O campo de Telefone deve ser preenchido!");
+        telefone.focus();
+        return false;
+    }
+
+    if (email.value.trim() === "") {
+        alert("O campo E-mail deve ser preenchido!");
+        email.focus();
+        return false;
+    }
+
+    if (contato.value.trim() === "") {
+        alert("O campo de Contato deve ser preenchido!");
+        contato.focus();
+        return false;
+    }
+   
+    // ================================================
+
+    if (nome_fornecedor.value.length < 3) {
+        alert("O nome deve ter pelo menos 3 caracteres!");
+        return false;
+    }
+
+    // Validação do e-mail
+    if (email.value.indexOf("@") === -1) {
+        alert("Digite um e-mail válido!");
+        return false;
+    }
+
+    if (telefone.value.length < 11) {
+        alert("Insira um número de telefone válido!");
+        telefone.focus();
+        return false;
+    }
+
+
+    // Se passou em todas as validações, envia o formulário
+    event.target.submit();
+}
+document.querySelector("form").addEventListener("submit", ValidacaodoFormularioFornecedor);
+
+
+
+
