cb4c76798ea7a233f9041d4a748ff88167e346f7
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-08-21 13:17:15 -0300

    professor, eu so estava visualizando as permissçoes e percebi que tinha cosias erradas 👍

--- ProvaPHP/principal.php
+++ ProvaPHP/principal.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 session_start();
-require_once 'conexao.php'; 
+require_once 'conexao.php';
 
-if(!isset($_SESSION['usuario'])) {
+if (!isset($_SESSION['usuario'])) {
     header("Location: index.php");
     exit();
 }
@@ -17,78 +17,93 @@ $nome_perfil = $perfil['nome_perfil'];
 
 // Definição das permissões por perfil
 $permissoes = [
-    1 =>["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php" ],
+    1 => [
+        "Cadastrar" => ["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
 
-         "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php" ],
+        "Buscar" => ["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
 
-         "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php" ],
+        "Alterar" => ["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
 
-         "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php" ]],
+        "Excluir" => ["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]
+    ],
 
-         2 =>["Cadastrar"=>["cadastro_cliente.php"],
+    2 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
 
-         "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
 
-         "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
 
-         "Excluir"=>["excluir_produto.php"]],
-         
-         3=> ["Cadastrar"=>["cadastro_produto.php","cadastro_fornecedor"],
+        "Excluir" => ["excluir_produto.php"]
+    ],
 
-         "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+   
+    3 => [
+        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
 
-         "Alterar"=>["alterar_perfil.php","alterar_produto.php","alterar_fornecedor.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
 
-         "Excluir"=>["excluir_produto.php","excluir_fornecedor.php"]],
-         
-         4=> ["Buscar"=>["buscar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
+        
+        "Excluir" => ["excluir_produto.php"]
+    ],
 
-         "Alterar"=>["alterar_perfil.php"]]];
+    4 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+
+        "Buscar" => ["buscar_cliente.php"],
+
+        "Alterar" => ["alterar_cliente.php"]],
+    
+    ];
 
 
-         ///Obtendo as opções disponíveis para o perfil logado
-         $opcoes_menu = $permissoes[$id_perfil];
+///Obtendo as opções disponíveis para o perfil logado
+$opcoes_menu = $permissoes[$id_perfil];
 ?>
 
 <!DOCTYPE html>
 <html lang="PT-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel principal</title>
-    <link rel="stylesheet" href="styles.css"/>
+    <link rel="stylesheet" href="styles.css" />
     <script src="scripts.js"></script>
 </head>
+
 <body>
     <header>
         <div class="saudacao">
-            <h2>Bem-Vindo, <?php echo $_SESSION['usuario'];?>! | Perfil: <?php echo $nome_perfil; ?></h2>
+            <h2>Bem-Vindo, <?php echo $_SESSION['usuario']; ?>! | Perfil: <?php echo $nome_perfil; ?></h2>
         </div>
         <div class="logout">
             <form action="logout.php" method="POST">
-                <button  type="submit">Logout</button>
+                <button type="submit">Logout</button>
             </form>
         </div>
-</header>
-<nav>
-    <ul class="menu">
-        <?php foreach($opcoes_menu as $categoria =>$arquivos) :?>
-            <li class="dropdown">
-                <a href="#"><?=$categoria ?></a>
-                <ul class="dropdown-menu">
-                    <?php foreach($arquivos as $arquivo) :?>
-                        <li>
-                           <a href="<?= $arquivo ?>"><?=ucfirst(str_replace("_"," ",basename($arquivo, ".php")))?></a>
-                        </li>
-                    <?php endforeach;?>
-                </ul>
-            </li>
-        <?php endforeach; ?>
-    </ul>  
-</nav>
-<address>
-    <br><br><br><br>
+    </header>
+    <nav>
+        <ul class="menu">
+            <?php foreach ($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?= $categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach ($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+    <address>
+        <br><br><br><br>
         Raquel Fernandes / Estudante / raquel_f_brito@estudante.sesisenai.org.br
- </address>
+    </address>
 </body>
+
 </html>
