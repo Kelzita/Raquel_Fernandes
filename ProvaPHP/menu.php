078de735b3ee628ed8af70a318cad0e09c6ee09e
Kelzita <raquelfernandesbriyoidjdj@gmail.com>
2025-08-22 15:30:48 -0300

    Menu
    Menu dropdown, permite a navegabilidade no site

--- ProvaPHP/menu.php
+++ ProvaPHP/menu.php
@@ -16,33 +16,44 @@ $nome_perfil = $perfil['nome_perfil'];
 
 // Definição das permissões por perfil
 $permissoes = [
-    1 =>["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php" ],
+    1 => [
+        "Cadastrar" => ["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
 
-         "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php" ],
+        "Buscar" => ["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
 
-         "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php" ],
+        "Alterar" => ["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
 
-         "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php" ]],
+        "Excluir" => ["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]
+    ],
 
-         2 =>["Cadastrar"=>["cadastro_cliente.php"],
+    2 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
 
-         "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
 
-         "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
 
-         "Excluir"=>["excluir_produto.php"]],
-         
-         3=> ["Cadastrar"=>["cadastro_produto.php","cadastro_fornecedor"],
+        "Excluir" => ["excluir_produto.php"]
+    ],
 
-         "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+   
+    3 => [
+        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
 
-         "Alterar"=>["alterar_perfil.php","alterar_produto.php","alterar_fornecedor.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
 
-         "Excluir"=>["excluir_produto.php","excluir_fornecedor.php"]],
-         
-         4=> ["Buscar"=>["buscar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
 
-         "Alterar"=>["alterar_perfil.php"]]];
+        "Excluir" => ["excluir_produto.php"]
+    ],
+ 
+    
+    4 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        
+        "Alterar" => ["alterar_cliente.php"]],
+    
+    ];
 
 
          ///Obtendo as opções disponíveis para o perfil logado
