a348748305a39e4c5b3a97ca7b75985bdf475100
Kelzita <raquelfernandesbriyoidjdj@gmail.com>
2025-08-22 13:41:14 -0300

    Cadastro Fornecedor
    Responsável por cadastrar o fornecedor

--- ProvaPHP/cadastro_fornecedor.php
+++ ProvaPHP/cadastro_fornecedor.php
@@ -39,6 +39,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Fornecedor</title>
     <link rel="stylesheet" href="styles.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script>
+        $(document).ready(function(){
+        $('#telefone').mask('(00) 00000-0000');
+    });
+    </script>
 </head>
 <body>
     <h2>Cadastrar Fornecedor</h2>
