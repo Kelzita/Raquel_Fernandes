72c28f95a89a82af904a01f2483896c1703395db
Kelzita <raquelfernandesbriyoidjdj@gmail.com>
2025-08-22 15:35:52 -0300

    Excluir Fornecedor
    Responsável por excluir o fornecedor

--- bat.php
+++ bat.php
@@ -1,36 +1,98 @@
 <?php 
-    session_start();
-    require 'conexao.php';
+session_start();
+require_once 'conexao.php';
+require_once 'menu.php';
 
-    if($_SESSION['perfil'] !=1){
-        echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
-        exit();
-    }
+//Verifica se o usuário tem permissão de adm
+if($_SESSION['perfil'] != 1) {
+    echo "<script>alert('Acesso negado!'); window.location.href='principal.php'</script>";
+    exit();
+}
 
-    if($_SERVER["REQUEST_METHOD"] == "POST");
-    $id_usuario = $_POST['id_usuario'];
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $id_perfil = $_POST['id_perfil'];
-    $nova_senha = !empty($_POST['nova_senha'])? password_hash($_POST['nova_senha'], PASSWORD_DEFAULT): null;
+//Inicializa variável par armazenar usuários
+$fornecedores = [];
 
-    
-    if($nova_senha){
-        $sql = "UPDATE usuario SET nome = :nome, email = :email, id_perfil = :id_perfil, senha = :senha WHERE id_usuario = :id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':senha', $nova_senha);
-    } else{
-        $sql = "UPDATE usuario SET nome = :nome, email = :email, id_perfil = :id_perfil WHERE id_usuario = :id";
-        $stmt = $pdo->prepare($sql);
-    }
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':id_perfil', $id_perfil);
-    $stmt->bindParam(':id', $id_usuario);
+//Buscar todos os usuário cadastrados em ordem alfabética
+
+$sql = "SELECT * FROM fornecedor ORDER BY nome_fornecedor ASC";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$fornecedores = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+//Se um id foi passado via get exclui o usuário
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id_fornecedor = $_GET['id'];
+
+    // Exclui o usuário do Banco de dados
+    $sql = "DELETE FROM fornecedor WHERE id_fornecedor = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id',$id_fornecedor, PDO::PARAM_INT);
 
     if($stmt->execute()) {
-        echo "<script>alert('Usuario atualizado com sucesso!');window.location.href='buscar_usuario.php';</script>";
-    } else{
-        echo "<script>alert('Erro ao atualizar usuario!');window.location.href='alterar_usuario.php?=$id_usuario';</script>";
+        echo "<script>alert('Fornecedor excluído com sucesso!'); window.location.href='excluir_fornecedor.php'</script>";
+        exit();
+    } else {
+        echo "<script>alert('Erro ao excluir o Fornecedor');</script>";
     }
+
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="tabela.css">
+    <title>Excluir Fornecedor</title>
+</head>
+<body>
+    <h2>Excluir Fornecedor</h2>
+    <?php if(!empty($usuarios)) : ?>
+        <table  border="1">
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Endereço</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+                <th>Contato</th>
+                <th>Ações</th>
+            </tr>
+
+            <?php foreach ($fornecedores as $fornecedor
+    ) : ?>
+                <tr>
+                    <td><?=htmlspecialchars($fornecedor
+            ['id_fornecedor'])?></td>
+                    <td><?=htmlspecialchars($fornecedor
+            ['nome_fornecedor'])?></td>
+                    <td><?=htmlspecialchars($fornecedor
+            ['endereco'])?></td>
+                    <td><?=htmlspecialchars($fornecedor
+            ['telefone'])?></td>
+                    <td><?=htmlspecialchars($fornecedor
+            ['email'])?></td>
+                    <td>
+                    <td><?=htmlspecialchars($fornecedor
+            ['contato'])?></td>
+                    <td>
+                        <a href="excluir_fornecedor.php?id=<?=htmlspecialchars($fornecedor['id_fornecedor'])?>" onclick="return confirm('Tem certeza que deseja excluir este fornecedor?')">Excluir</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php else : ?>
+            <p>Nenhum fornecedor encontrado</p>
+        <?php endif; ?>
+        
+        <a href="principal.php">Voltar</a>
+        <address>
+    <br><br><br><br>
+        Raquel Fernandes / Estudante / raquel_f_brito@estudante.sesisenai.org.br
+ </address>
+   
+</body>
+</html>
